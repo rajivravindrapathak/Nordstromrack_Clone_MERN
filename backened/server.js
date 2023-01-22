39f8a770072640d35bbbbec50b4f1abc3bfe58b8
Rajiv Ravindra pathak <70104100+rajivravindrapathak@users.noreply.github.com>
2023-01-22 21:50:17 +0530

    Add files via upload

--- backened/server.js
+++ backened/server.js
@@ -6,41 +6,48 @@ const { UserModel } = require("./models/UserModel")
 require("dotenv").config()
 
 const app = express()
+
+app.use(cors())
 app.use(express.json())
+
 app.get('/', (req, res) => {
     res.send("nordstromrack home page")
 })
 
-// build signup api
-app.post('/signup', async (req, res) => {
-    const {fname, lname, email, password} = req.body
+
+// backened signup api
+app.post("/signup", async (req, res) => {
+    const {name, email, password} = req.body
+    // if user already signup it means user alrady exits please try login
     const isUser = await UserModel.findOne({email})
     if(isUser) {
-        res.send("user already exit please login again") 
+        res.send({"msg" : "user already exits please login"})
     }
-    bcrypt.hash(password, 4, async function(err, hash) {
-        if(err) {
-            console.log("something went wrong, please try again")
-        }
-        else {
-             const new_user = new UserModel({
-                fname,
-                lname,
-                email,
-                password: hash
-             })
-             try {
-                await new_user.save()
-                res.send('signup successful')
-             } catch (err) {
-                res.send('something went wrong')
-             }
-        }
-    })
+    else {
+        bcrypt.hash(password, 4, async function(err, hash) {
+            if(err) {
+                res.send("something went wrong, please try again")
+            } 
+            else {
+                const new_user = new UserModel({
+                    name,
+                    email,
+                    password : hash
+                })
+            
+                try {
+                    await new_user.save()
+                    res.send({"msg" : "sign up successfull"})
+                } catch(err) {
+                    res.send({"msg" : "something went wrong, please try again"})
+                }
+            }
+        });
+    }
 })
 
-// build login api
-app.post('/login', async (req, res) => {
+// backend login api
+app.post("/login", async (req, res) => {
     const {email, password} = req.body
     const user = await UserModel.findOne({email})
     const hashed_password = user.password
@@ -49,16 +56,17 @@ app.post('/login', async (req, res) => {
     console.log(user_id)
     bcrypt.compare(password, hashed_password, function(err, result) {
         if(err) {
-            res.send("something went wrong, please try again later")
+            res.send({"msg": "something went wrong, please try again later"})
         }
         if(result) {
             const token = jwt.sign({user_id}, process.env.SECRET_KEY)
             res.send({message : "login successful", token})
         } else {
-            res.send("login failed")
-        }
-    });
+            res.send({"msg" : "login failed"})
+        } 
+    }); 
 })
+
 
 // listining on port and connected db
 const PORT = 8001
