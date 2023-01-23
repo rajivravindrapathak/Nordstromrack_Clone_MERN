de77450c5b43d0a34b63c4fa61ebec27d47c145c
Rajiv Ravindra pathak <70104100+rajivravindrapathak@users.noreply.github.com>
2023-01-23 14:31:56 +0530

    Add files via upload

---
+++ fronted/src/Components/AlRoutes.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Route } from "react-router-dom";
+import { Routes } from "react-router-dom";
+
+const AllRoutes = () => {
+    return ( 
+        <div>
+            <Routes>
+                <Route />
+            </Routes>
+        </div>
+     );
+}
+ 
+export default AllRoutes;
